9eeac45d51ac343392cb412764e183bd897e033b
Harry Wolff <hswolff@gmail.com>
2013-07-03 23:25:32 -0400

    rename

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bringthesalsaApp', [])
+angular.module('BringTheSalsaApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bringthesalsaApp')
+angular.module('BringTheSalsaApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
