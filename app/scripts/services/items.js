4e166254c4ed416bb2b8e45a224215b101f4f455
Harry Wolff <hswolff@gmail.com>
2013-07-06 00:48:18 -0400

    create default values for new items added

--- app/scripts/services/items.js
+++ app/scripts/services/items.js
@@ -9,18 +9,23 @@ var defaultItems = [
 var localStorageItems = angular.fromJson(localStorage.getItem('items'));
 
 var ItemCollection = function() {
-  this.models = localStorageItems.length ? localStorageItems : defaultItems;
+  this.models = localStorageItems && localStorageItems.length ? localStorageItems : defaultItems;
   angular.forEach(this.models, function(m) {
     m.currentlyEditing = false;
   });
 };
 
 ItemCollection.prototype.add = function(model) {
-  if (angular.isString(model)) {
-    model = {'what': model};
+  function formatModel(model) {
+    if (angular.isString(model)) {
+      model = {'what': model};
+    }
+    // model.currentlyEditing = 'all';
+    model.quantity = 1;
+    model.who = 'Someone bring this!';
+    return model;
   }
-  model.currentlyEditing = 'all';
-  this.models.unshift(model);
+  this.models.unshift(formatModel(model));
   return model;
 };
 
