7b77b144bd37d2ad7dd7f134db3bc88c5fca941f
Harry Wolff <hswolff@gmail.com>
2013-07-07 19:39:49 -0400

    optimize dist task

--- Gruntfile.js
+++ Gruntfile.js
@@ -243,7 +243,6 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
-            'bower_components/**/*',
             'images/{,*/}*.{gif,webp,svg}',
             'styles/fonts/*'
           ]
